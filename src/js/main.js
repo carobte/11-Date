0bb30efae1a8c23a3defcb0bfce57b9917081e02
carobte <caro.bustamante.escobar@gmail.com>
2024-04-26 13:28:41 -0500

    ejercicios

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@ console.log("Date")
 
 // new Date -> devuelve la fecha actual, mes comienza en 0
 
-console.log(new Date()) 
+// console.log(new Date()) 
 // se le pueden pasar parametros: año, mes, dia
 // como string: "mes/dia/año" y el mes comienza en 1 normal
 
 // Métodos: 
 
-const fecha = new Date()
+/* const fecha = new Date()
 
 const day = fecha.getDate() // numero del mes
 const weekDay = fecha.getDay() // 0-6 -> de la semana, domingo es el 0
@@ -28,6 +28,38 @@ const usuario = prompt("Ingrese una fecha: ", "MM/DD/YYYY")
 
 const fechaUsuario = new Date(usuario)
 
-console.log(fechaUsuario)
+console.log(fechaUsuario) */
 
-// Operaciones con fechas
+/* // Ejercicio 1 -> Biblioteca
+
+
+const diasPrestamo = Number(prompt("Ingrese la cantidad de días que prestará el libro"))
+
+const fechaDevolucion = new Date()
+
+fechaDevolucion.setDate(fechaDevolucion.getDate() + diasPrestamo)
+
+console.log(fechaDevolucion) */
+
+/* // Ejercicio 2 -> Edad
+
+const fechaBD = new Date(prompt("Ingrese su fecha de nacimiento", "MM/DD/YYYY"))
+
+const fechaActual = new Date()
+
+const edad = fechaActual.getFullYear() - fechaBD.getFullYear()
+
+const edad2 = (fechaActual - fechaBD) / (1000*60*60*24*365)
+
+console.log(edad) */
+
+/* // Ejercicio 3 -> Cumpleaños
+
+const fechaActual = new Date()
+
+const fechaBD = new Date(prompt("Ingrese su fecha de nacimiento", "MM/DD/YYYY"))
+
+const mesesRestantes = fechaBD.getMonth() - fechaActual.getMonth() 
+
+console.log(`Faltan: ${mesesRestantes} meses para el cumpleaños`)
+ */
